e615f2cd1152b576d72c61cafe81e58d1b8e5ae3
Shashank Shekhar <shashankshekharlkrmit1@gmail.com>
2023-11-19 12:20:38 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
-function App() {
+import Button from '@mui/material/Button';
+const App = ()=>{
+
+  const [num_value,setnum_value] = useState(0);
+
+  const increm = ()=>{
+    return(setnum_value(num_value+1))
+  }
+
+  const decrem = ()=>{
+    if (num_value<=0){
+      return(alert("Limit reached : 0"))
+    }
+    else {
+    return(setnum_value(num_value-1))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center_box">
+      <h2>{num_value}</h2>
+      <div className="buttons">
+      <Button  onClick={increm} variant="contained" color="success">
+        <AddIcon/>
+      </Button>
+      <Button onClick={decrem} variant="outlined" color="error">
+        <RemoveIcon/>
+      </Button>  
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
